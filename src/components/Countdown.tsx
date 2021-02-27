e05b02bea4af042ee6019f65f5729986cba9a699
gustavos00 <z.gazevedo20@gmail.com>
2021-02-27 14:17:47 -0100

    Atualizaçao da tela principal e do sistema de XP.

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,9 +1,16 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown() {
-  const [time, setTime] = useState(25 * 60);
-  const [active, setActive] = useState(false);
+  const { startNewChallenge } = useContext(ChallengesContext);
+
+  const [time, setTime] = useState(0.1 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
+
+  let countdownTimeout: NodeJS.Timeout;
 
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
@@ -24,16 +31,27 @@ export function Countdown() {
     .split('');
 
   function startCountdown() {
-    setActive(true);
+    setIsActive(true);
+  }
+
+  function resetCountdown() {
+    clearTimeout(countdownTimeout);
+
+    setIsActive(false);
+    setTime(0.1 * 60);
   }
 
   useEffect(() => {
-    if (active && time > 0) {
-      setTimeout(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
         setTime(time - 1);
       }, 1000);
+    } else if (isActive && time == 0) {
+      setHasFinished(true);
+      setIsActive(false);
+      startNewChallenge();
     }
-  }, [active, time]);
+  }, [isActive, time]);
 
   return (
     <div>
@@ -51,13 +69,32 @@ export function Countdown() {
         </div>
       </div>
 
-      <button
-        type="button"
-        className={styles.countdownButton}
-        onClick={startCountdown}
-      >
-        Iniciar um ciclo
-      </button>
+      {hasFinished ? (
+        <button disabled className={styles.countdownButton}>
+          Ciclo Terminado
+        </button>
+      ) : (
+        /*<> = Chamasse "Fragmetent". É usado devido a limitação do react em relação aos objetos sem uma div em volta. */
+        <>
+          {isActive ? (
+            <button
+              type="button"
+              className={` ${styles.countdownButton} ${styles.countdownButtonActive}`}
+              onClick={resetCountdown}
+            >
+              Pausar ciclo
+            </button>
+          ) : (
+            <button
+              type="button"
+              className={styles.countdownButton}
+              onClick={startCountdown}
+            >
+              Iniciar um ciclo
+            </button>
+          )}
+        </>
+      )}
     </div>
   );
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,10 +1,14 @@
-import styles from '../styles/components/Profile.module.css'
+import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
   return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/gustavos00.png" alt="Gustavo Santos image" />
-
+      <a href="https://github.com/gustavos00">
+        <img
+          src="https://github.com/gustavos00.png"
+          alt="Gustavo Santos image"
+        />
+      </a>
       <div>
         <strong>Gustavo Santos</strong>
 
